49bc989e9bdb6781591fe44055e141b543a34b84
Ricardo <tricardo003@gmail.com>
2020-04-03 16:32:12 -0500

    done from-event metod

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { fromEvent, interval } from 'rxjs';
+import { map, buffer, throttle, filter, debounceTime } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'prueba1';
+export class AppComponent implements OnInit {
+  ngOnInit(): void {
+    const eventoClick$ = fromEvent(
+      document.getElementById('btnclick'),
+      'click'
+    );
+    console.log(eventoClick$);
+
+    let contador = 0;
+    const click$ = eventoClick$.pipe(
+      buffer(eventoClick$.pipe(throttle((ev) => interval(250)))),
+      map((list) => {
+        return list.length;
+      }),
+      filter((x) => {
+        return x >= 2;
+      })
+    );
+
+    click$.subscribe(() => {
+      contador++;
+      const numeroClick = document.getElementById('message');
+      numeroClick.innerHTML = contador.toString();
+
+      console.log('Diste mas clicks');
+      console.log(contador);
+    });
+  }
+
+  updateResult(type: string) {}
 }
+// this.button = document.getElementById('link');
+// fromEvent(this.button, 'click').subscribe(event => console.log(event));
